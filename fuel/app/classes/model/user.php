a701a993bf51c1e3a7409226aae09b82af7e9a46
tomato <tomatono@gmail.com>
2022-02-08 18:38:10 +0900

    created_at, updated_at をタイムスタンプにする

--- fuel/app/classes/model/user.php
+++ fuel/app/classes/model/user.php
@@ -17,11 +17,11 @@ class Model_User extends Model
 	protected static $_observers = array(
 		'Orm\Observer_CreatedAt' => array(
 			'events' => array('before_insert'),
-			'mysql_timestamp' => false,
+			'mysql_timestamp' => true,
 		),
 		'Orm\Observer_UpdatedAt' => array(
 			'events' => array('before_save'),
-			'mysql_timestamp' => false,
+			'mysql_timestamp' => true,
 		),
 	);
 
