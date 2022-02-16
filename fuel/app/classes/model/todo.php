e6df91b0ba0bbaf3de3f8bf2c4e71c7ca05d4d93
tomato <tomatono@gmail.com>
2022-02-16 16:18:24 +0900

    修正

--- fuel/app/classes/model/todo.php
+++ fuel/app/classes/model/todo.php
@@ -1,42 +1,44 @@
 <?php
+
 use Orm\Model;
+use Fuel\Core\Validation;
 
 class Model_Todo extends Model
 {
-	protected static $_properties = array(
-		'id',
-		'title',
-		'comment',
-		'state',
-		'due_date',
-		'due_time',
-		'created_user',
-		'updated_user',
-		'created_at',
-		'updated_at',
-	);
+    protected static $_properties = array(
+        'id',
+        'title',
+        'comment',
+        'state',
+        'due_date',
+        'due_time',
+        'created_user',
+        'updated_user',
+        'created_at',
+        'updated_at',
+    );
 
-	protected static $_observers = array(
-		'Orm\Observer_CreatedAt' => array(
-			'events' => array('before_insert'),
-			'mysql_timestamp' => true,
-		),
-		'Orm\Observer_UpdatedAt' => array(
-			'events' => array('before_save'),
-			'mysql_timestamp' => true,
-		),
-	);
+    protected static $_observers = array(
+        'Orm\Observer_CreatedAt' => array(
+            'events' => array('before_insert'),
+            'mysql_timestamp' => true,
+        ),
+        'Orm\Observer_UpdatedAt' => array(
+            'events' => array('before_save'),
+            'mysql_timestamp' => true,
+        ),
+    );
 
-	public static function validate($factory)
-	{
-		$val = Validation::forge($factory);
-		$val->add_field('title', 'Title', 'required|max_length[200]');
-		$val->add_field('comment', 'Comment', 'required');
-		$val->add_field('state', 'State', 'valid_string[numeric]|max_length[1]');
-		$val->add('due_date', 'Due Date')->add_rule('valid_date', 'Y-m-d');
-		$val->add_field('due_time', 'Due Time');
+    public static function validate($factory)
+    {
+        $val = Validation::forge($factory);
+        $val->add_field('title', 'Title', 'required|max_length[200]');
+        $val->add_field('comment', 'Comment', 'required');
+        $val->add_field('state', 'State', 'valid_string[numeric]|max_length[1]');
+        $val->add('due_date', 'Due Date')->add_rule('valid_date', 'Y-m-d');
+        $val->add_field('due_time', 'Due Time', null);
 
-		return $val;
-	}
+        return $val;
+    }
 
 }
